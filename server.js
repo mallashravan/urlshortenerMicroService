7857b8b5b75b4c7245912462efe8919b80d4b5c8
Glitch (sleet-authorization) <none>
2019-06-18 13:47:31 +0000

    💴🎁 Checkpoint ./server.js:2582873/378 ./package.json:2582873/1069

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
@@ -12,26 +12,30 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+console.log(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
-
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
   
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.post("/api/shorturl/new",function(req,res)
+        {
+    var payload = req.body;
+  res.json(payload);
+});
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
