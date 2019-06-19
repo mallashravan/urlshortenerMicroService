352f7d72300a8c22c55d3a4c683a1e189dd43ce4
Glitch (sleet-authorization) <none>
2019-06-19 08:42:07 +0000

    🌵✂️ Checkpoint ./package.json:2582873/1133 ./server.js:2582873/4428

--- server.js
+++ server.js
@@ -2,31 +2,35 @@
 
 var express = require('express');
 var mongo = require('mongodb');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var dns = require('dns');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://malla_shravan:08121989@cluster0-shgf3.mongodb.net/test?retryWrites=true&w=majority';
+
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 console.log(process.env.MONGOLAB_URI);
-mongoose.connect('mongodb+srv://malla_shravan:08121989@cluster0-shgf3.mongodb.net/test?retryWrites=true&w=majority');
 app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 app.use(bodyParser.urlencoded());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-var Schema = mongoose.Schema;
-  var urlSchema = new Schema({
-     original_url: String,
-     short_url: Number 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+    var dbo = db.db("mydb");
+dbo.createCollection("urls", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    dbo.close();
   });
-  var Url = mongoose.model('Url', urlSchema);
-
+  db.close();
+});
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -56,4 +60,5 @@ app.post("/api/shorturl/new",function(req,res)
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
+
 
